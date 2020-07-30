8f09bcc0025879eca3ff83553c7b0399509d77ac
aaron <aaron@192.168.0.59>
2020-07-30 16:46:33 +0100

    User getter and setters

--- src/accounts/User.java
+++ src/accounts/User.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package accounts;
 
 import java.util.HashMap;
@@ -18,5 +14,70 @@ public class User
     HashMap<String,Account> friends = new HashMap<>();
     HashMap<String,Account> mutualFriends = new HashMap<>();
     HashMap<String,Post> posts = new HashMap<>();
-    boolean accountPrivate;
+    
+    public User(String name,String accountId,HashMap<String,Account> friends, HashMap<String,Account> mutalFriends,HashMap<String,Post> posts)
+    {
+        this.name = name;
+        this.accountId = accountId;
+        this.friends = friends; 
+        this.mutualFriends = mutalFriends;
+        this.posts = posts;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getAccountId()
+    {
+        return accountId;
+    }
+
+    public HashMap<String, Account> getFriends()
+    {
+        return friends;
+    }
+
+    public HashMap<String, Account> getMutualFriends()
+    {
+        return mutualFriends;
+    }
+
+    public HashMap<String, Post> getPosts()
+    {
+        return posts;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setAccountId(String accountId)
+    {
+        this.accountId = accountId;
+    }
+
+    public void setFriends(HashMap<String, Account> friends)
+    {
+        this.friends = friends;
+    }
+
+    public void setMutualFriends(HashMap<String, Account> mutualFriends)
+    {
+        this.mutualFriends = mutualFriends;
+    }
+
+    public void setPosts(HashMap<String, Post> posts)
+    {
+        this.posts = posts;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "User{" + "name=" + this.getName() + ", accountId=" + this.getAccountId() + ", friends=" + this.getFriends() + ", mutualFriends=" + this.getMutualFriends() + ", posts=" + this.getPosts() + '}';
+    }
+    
 }
